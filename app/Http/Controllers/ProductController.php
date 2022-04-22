27e5ec17f9b468c053576ac7132258bfa2535216
Kingsley Amaitsa <mangwels@Kingsleys-MacBook-Pro.local>
2022-04-23 00:04:29 +0300

    Product api resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,12 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use \App\Models\Product;
-
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
     public function index()
     {
         return Product::all();
+    }
+
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+
+    public function store(Request $request)
+    {
+       // dd($request->all());
+        $product = Product::create($request->only('title','image'));
+
+        return response($product, Response::HTTP_CREATED);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        $product->update($request->only('title','image'));
+
+        return response($product, Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::destroy($id);
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::get('products', [ProductController::class, 'index']);
+Route::apiResource('products', ProductController::class);
